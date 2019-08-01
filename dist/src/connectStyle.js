38043a1b92bbf8924af2b8ca73316c7be0475e0d
Doug Koehn <dkoehn@churchcommunitybuilder.com>
2019-08-01 10:28:51 -0600

    dk - Built

--- dist/src/connectStyle.js
+++ dist/src/connectStyle.js
@@ -1,14 +1,22 @@
-Object.defineProperty(exports,"__esModule",{value:true});var _extends=Object.assign||function(target){for(var i=1;i<arguments.length;i++){var source=arguments[i];for(var key in source){if(Object.prototype.hasOwnProperty.call(source,key)){target[key]=source[key];}}}return target;};var _jsxFileName="src/connectStyle.js";var _createClass=function(){function defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value"in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor);}}return function(Constructor,protoProps,staticProps){if(protoProps)defineProperties(Constructor.prototype,protoProps);if(staticProps)defineProperties(Constructor,staticProps);return Constructor;};}();var _react=require("react");var _react2=_interopRequireDefault(_react);
-var _propTypes=require("prop-types");var _propTypes2=_interopRequireDefault(_propTypes);
-var _hoistNonReactStatics=require("hoist-non-react-statics");var _hoistNonReactStatics2=_interopRequireDefault(_hoistNonReactStatics);
-var _lodash=require("lodash");var _=_interopRequireWildcard(_lodash);
-var _normalizeStyle=require("./StyleNormalizer/normalizeStyle");var _normalizeStyle2=_interopRequireDefault(_normalizeStyle);
-var _reactNative=require("react-native");
+Object.defineProperty(exports,"__esModule",{value:true});var _extends=Object.assign||function(target){for(var i=1;i<arguments.length;i++){var source=arguments[i];for(var key in source){if(Object.prototype.hasOwnProperty.call(source,key)){target[key]=source[key];}}}return target;};var _jsxFileName='src/connectStyle.js';var _createClass=function(){function defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value"in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor);}}return function(Constructor,protoProps,staticProps){if(protoProps)defineProperties(Constructor.prototype,protoProps);if(staticProps)defineProperties(Constructor,staticProps);return Constructor;};}();exports.
 
-var _Theme=require("./Theme");var _Theme2=_interopRequireDefault(_Theme);
-var _resolveComponentStyle=require("./resolveComponentStyle");function _interopRequireWildcard(obj){if(obj&&obj.__esModule){return obj;}else{var newObj={};if(obj!=null){for(var key in obj){if(Object.prototype.hasOwnProperty.call(obj,key))newObj[key]=obj[key];}}newObj.default=obj;return newObj;}}function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}function _toConsumableArray(arr){if(Array.isArray(arr)){for(var i=0,arr2=Array(arr.length);i<arr.length;i++){arr2[i]=arr[i];}return arr2;}else{return Array.from(arr);}}function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self,call){if(!self){throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call&&(typeof call==="object"||typeof call==="function")?call:self;}function _inherits(subClass,superClass){if(typeof superClass!=="function"&&superClass!==null){throw new TypeError("Super expression must either be null or a function, not "+typeof superClass);}subClass.prototype=Object.create(superClass&&superClass.prototype,{constructor:{value:subClass,enumerable:false,writable:true,configurable:true}});if(superClass)Object.setPrototypeOf?Object.setPrototypeOf(subClass,superClass):subClass.__proto__=superClass;}
 
-var themeCache={};
+
+
+
+
+
+
+
+
+
+
+
+
+
+clearThemeCache=clearThemeCache;var _react=require('react');var _react2=_interopRequireDefault(_react);var _propTypes=require('prop-types');var _propTypes2=_interopRequireDefault(_propTypes);var _hoistNonReactStatics=require('hoist-non-react-statics');var _hoistNonReactStatics2=_interopRequireDefault(_hoistNonReactStatics);var _lodash=require('lodash');var _=_interopRequireWildcard(_lodash);var _normalizeStyle=require('./StyleNormalizer/normalizeStyle');var _normalizeStyle2=_interopRequireDefault(_normalizeStyle);var _reactNative=require('react-native');var _Theme=require('./Theme');var _Theme2=_interopRequireDefault(_Theme);var _resolveComponentStyle=require('./resolveComponentStyle');function _interopRequireWildcard(obj){if(obj&&obj.__esModule){return obj;}else{var newObj={};if(obj!=null){for(var key in obj){if(Object.prototype.hasOwnProperty.call(obj,key))newObj[key]=obj[key];}}newObj.default=obj;return newObj;}}function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}function _toConsumableArray(arr){if(Array.isArray(arr)){for(var i=0,arr2=Array(arr.length);i<arr.length;i++){arr2[i]=arr[i];}return arr2;}else{return Array.from(arr);}}function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self,call){if(!self){throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call&&(typeof call==="object"||typeof call==="function")?call:self;}function _inherits(subClass,superClass){if(typeof superClass!=="function"&&superClass!==null){throw new TypeError("Super expression must either be null or a function, not "+typeof superClass);}subClass.prototype=Object.create(superClass&&superClass.prototype,{constructor:{value:subClass,enumerable:false,writable:true,configurable:true}});if(superClass)Object.setPrototypeOf?Object.setPrototypeOf(subClass,superClass):subClass.__proto__=superClass;}var themeCache={};function clearThemeCache(){
+themeCache={};
+}
 
 
 
@@ -18,7 +26,7 @@ var themeCache={};
 
 function throwConnectStyleError(errorMessage,componentDisplayName){
 throw Error(
-errorMessage+" - when connecting "+componentDisplayName+" component to style.");
+errorMessage+' - when connecting '+componentDisplayName+' component to style.');
 
 }
 
@@ -176,7 +184,7 @@ style:finalStyle,
 addedProps:_this.resolveAddedProps(),
 styleNames:styleNames};return _this;
 
-}_createClass(StyledComponent,[{key:"getFinalStyle",value:function getFinalStyle(
+}_createClass(StyledComponent,[{key:'getFinalStyle',value:function getFinalStyle(
 
 props,context,style,styleNames){
 var resolvedStyle={};
@@ -203,7 +211,7 @@ return[concreteStyle,style];
 }
 
 return concreteStyle;
-}},{key:"getStyleNames",value:function getStyleNames(
+}},{key:'getStyleNames',value:function getStyleNames(
 
 props){
 var styleNamesArr=_.map(props,function(value,key){
@@ -218,7 +226,7 @@ return value===false;
 });
 
 return styleNamesArr;
-}},{key:"getParentPath",value:function getParentPath()
+}},{key:'getParentPath',value:function getParentPath()
 
 {
 if(!this.context.parentPath){
@@ -230,7 +238,7 @@ componentStyleName],_toConsumableArray(
 this.getStyleNames(this.props)));
 
 }
-}},{key:"getChildContext",value:function getChildContext()
+}},{key:'getChildContext',value:function getChildContext()
 
 {
 return{
@@ -240,7 +248,7 @@ return{
 
 parentPath:this.getParentPath()};
 
-}},{key:"componentWillReceiveProps",value:function componentWillReceiveProps(
+}},{key:'UNSAFE_componentWillReceiveProps',value:function UNSAFE_componentWillReceiveProps(
 
 nextProps,nextContext){
 var styleNames=this.getStyleNames(nextProps);
@@ -259,13 +267,13 @@ style:finalStyle,
 styleNames:styleNames});
 
 }
-}},{key:"setNativeProps",value:function setNativeProps(
+}},{key:'setNativeProps',value:function setNativeProps(
 
 nativeProps){
 if(this.wrappedInstance.setNativeProps){
 this.wrappedInstance.setNativeProps(nativeProps);
 }
-}},{key:"setWrappedInstance",value:function setWrappedInstance(
+}},{key:'setWrappedInstance',value:function setWrappedInstance(
 
 component){
 if(component&&component._root){
@@ -274,7 +282,7 @@ this._root=component._root;
 this._root=component;
 }
 this.wrappedInstance=this._root;
-}},{key:"hasStyleNameChanged",value:function hasStyleNameChanged(
+}},{key:'hasStyleNameChanged',value:function hasStyleNameChanged(
 
 nextProps,styleNames){
 return(
@@ -284,7 +292,7 @@ this.props!==nextProps&&
 
 !_.isEqual(this.state.styleNames,styleNames));
 
-}},{key:"shouldRebuildStyle",value:function shouldRebuildStyle(
+}},{key:'shouldRebuildStyle',value:function shouldRebuildStyle(
 
 nextProps,nextContext,styleNames){
 return(
@@ -294,7 +302,7 @@ nextContext.theme!==this.context.theme||
 !_.isEqual(nextContext.parentPath,this.context.parentPath)||
 this.hasStyleNameChanged(nextProps,styleNames));
 
-}},{key:"resolveStyleNames",value:function resolveStyleNames(
+}},{key:'resolveStyleNames',value:function resolveStyleNames(
 
 props){var
 styleName=props.styleName;
@@ -306,7 +314,7 @@ return styleNames;
 
 
 return _.uniq(mapPropsToStyleNames(styleNames,props));
-}},{key:"resolveAddedProps",value:function resolveAddedProps()
+}},{key:'resolveAddedProps',value:function resolveAddedProps()
 
 {
 var addedProps={};
@@ -314,7 +322,7 @@ if(options.withRef){
 addedProps.ref=this.setWrappedInstance;
 }
 return addedProps;
-}},{key:"getOrSetStylesInCache",value:function getOrSetStylesInCache(
+}},{key:'getOrSetStylesInCache',value:function getOrSetStylesInCache(
 
 context,props,styleNames,path){
 if(themeCache&&themeCache[path.join(">")]){
@@ -322,13 +330,13 @@ if(themeCache&&themeCache[path.join(">")]){
 
 return themeCache[path.join(">")];
 }else{
-resolvedStyle=this.resolveStyle(context,props,styleNames);
+var resolvedStyle=this.resolveStyle(context,props,styleNames);
 if(Object.keys(themeCache).length<10000){
 themeCache[path.join(">")]=resolvedStyle;
 }
 return resolvedStyle;
 }
-}},{key:"resolveStyle",value:function resolveStyle(
+}},{key:'resolveStyle',value:function resolveStyle(
 
 context,props,styleNames){
 var parentStyle={};
@@ -356,7 +364,7 @@ styleNames,
 themeStyle,
 parentStyle);
 
-}},{key:"resolveConnectedComponentStyle",value:function resolveConnectedComponentStyle(
+}},{key:'resolveConnectedComponentStyle',value:function resolveConnectedComponentStyle(
 
 
 
@@ -369,7 +377,7 @@ props){
 var styleNames=this.resolveStyleNames(props);
 return this.resolveStyle(this.context,props,styleNames).
 componentStyle;
-}},{key:"render",value:function render()
+}},{key:'render',value:function render()
 
 {var _state=
 this.state,addedProps=_state.addedProps,style=_state.style;
@@ -378,10 +386,10 @@ return(
 _react2.default.createElement(WrappedComponent,_extends({},
 this.props,
 addedProps,{
-style:style,__source:{fileName:_jsxFileName,lineNumber:378}})));
+style:style,__source:{fileName:_jsxFileName,lineNumber:386}})));
 
 
-}}]);return StyledComponent;}(_react2.default.PureComponent);StyledComponent.contextTypes={theme:_Theme.ThemeShape,parentPath:_propTypes2.default.array};StyledComponent.childContextTypes={parentPath:_propTypes2.default.array};StyledComponent.propTypes={style:_propTypes2.default.oneOfType([_propTypes2.default.object,_propTypes2.default.number,_propTypes2.default.array]),styleName:_propTypes2.default.string,virtual:_propTypes2.default.bool};StyledComponent.defaultProps={virtual:options.virtual};StyledComponent.displayName="Styled("+componentDisplayName+")";StyledComponent.WrappedComponent=WrappedComponent;
+}}]);return StyledComponent;}(_react2.default.PureComponent);StyledComponent.contextTypes={theme:_Theme.ThemeShape,parentPath:_propTypes2.default.array};StyledComponent.childContextTypes={parentPath:_propTypes2.default.array};StyledComponent.propTypes={style:_propTypes2.default.oneOfType([_propTypes2.default.object,_propTypes2.default.number,_propTypes2.default.array]),styleName:_propTypes2.default.string,virtual:_propTypes2.default.bool};StyledComponent.defaultProps={virtual:options.virtual};StyledComponent.displayName='Styled('+componentDisplayName+')';StyledComponent.WrappedComponent=WrappedComponent;
 
 
 return(0,_hoistNonReactStatics2.default)(StyledComponent,WrappedComponent);
